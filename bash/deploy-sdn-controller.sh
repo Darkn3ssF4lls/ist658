5142d0209d956c5c5b538d44960d15e549d7a170
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-26 16:06:48 -0500

    Inserted copy of simple_switch from ryu. Added more pacakages from guides for Ryu SDN instal.

--- bash/deploy-sdn-controller.sh
+++ bash/deploy-sdn-controller.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 #This script deploys the packages required to run a RYU SDN controller
-sudo apt install python3-pip gcc python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev
+sudo apt install python3-pip gcc python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev libevent-dev python-all-dev
 
 #Required packages and optional packages as slected by  https://ryu.readthedocs.io/en/latest/getting_started.html
 sudo pip3 install ryu
 sudo pip3 install ncclient
 sudo pip3 install paramiko
 sudo pip3 install SQLAlchemy
+sudo pip3 install WeBOB
+sudo pip3 install tinyrpc
+sudo pip3 install routes
+sudo pip3 install ovs 
+sudo pip3 install oslo.config
+sudo pip3 install msgpack
+sudo pip3 install eventlet
+

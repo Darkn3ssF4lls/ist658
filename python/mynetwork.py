65450d7a0d25ed626100eefb9fc092ea7bb6c715
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-26 10:51:18 -0500

    Updated  mynetwork.py with the language to actually perform some sdn actions.

--- python/mynetwork.py
+++ python/mynetwork.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 #This is the first script to setting up my SDN with  RYU.
+#Created using the examples from https://ryu.readthedocs.io/en/latest/writing_ryu_app.html
 from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller import MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_0
 
 class L2Switch(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
+
     def _init_(self, *args, **kwargs):
        super(L2Switch, self)._init(*args, **kwargs)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        dp = msg.datapath
+        ofp = dp.ofproto
+        ofp_parser = dp.ofproto_parser
+
+        actions = [ofp_parser.OFPActionOutput(ofp.OFPP_FLOOD)]
+        out = ofp_parser.OFPPacketOut(
+            datapath=dp, buffer_id=msg.buffer_id, in_port=msg.in_port, actions=actions)
+        dp.send_msg(out)

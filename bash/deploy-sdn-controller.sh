9beb1cca5a698489fd9c110318c8a147722b8a0e
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-26 10:03:28 -0500

    Generated script to download required software to depkoy and manage a SDN with Ryu.

---
+++ bash/deploy-sdn-controller.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#This script deploys the packages required to run a RYU SDN controller
+sudo apt install python-pip
+sudo apt install python3-pip
+
+sudo pip install ryu
+sudo pip3 install ryu
+ 
+

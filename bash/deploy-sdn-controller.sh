f052d6b7bd6de97ad4d5ab3393927ec1f2ec3e04
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-26 10:22:19 -0500

    Updated deploy script to include all the packages required and optional for a RYU deployment

--- bash/deploy-sdn-controller.sh
+++ bash/deploy-sdn-controller.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #This script deploys the packages required to run a RYU SDN controller
-sudo apt install python-pip
-sudo apt install python3-pip
+sudo apt install python3-pip gcc python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev
 
-sudo pip install ryu
+#Required packages and optional packages as slected by  https://ryu.readthedocs.io/en/latest/getting_started.html
 sudo pip3 install ryu
- 
-
+sudo pip3 install ncclient
+sudo pip3 install paramiko
+sudo pip3 install SQLAlchemy

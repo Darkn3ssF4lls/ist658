bfe32abfd8cb238fb170fd089d49e46a80ee3db8
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-26 10:58:14 -0500

    Updated mynetwork.py with a copy paste from the tutorial. Was missing a few lines, wanted to make sure it works to move forward with learning about SDN.

--- python/mynetwork.py
+++ python/mynetwork.py
@@ -3,15 +3,15 @@
 #Created using the examples from https://ryu.readthedocs.io/en/latest/writing_ryu_app.html
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller import MAIN_DISPATCHER
+from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_0
 
 class L2Switch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
 
-    def _init_(self, *args, **kwargs):
-       super(L2Switch, self)._init(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(L2Switch, self).__init__(*args, **kwargs)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, ev):
@@ -22,5 +22,6 @@ class L2Switch(app_manager.RyuApp):
 
         actions = [ofp_parser.OFPActionOutput(ofp.OFPP_FLOOD)]
         out = ofp_parser.OFPPacketOut(
-            datapath=dp, buffer_id=msg.buffer_id, in_port=msg.in_port, actions=actions)
+            datapath=dp, buffer_id=msg.buffer_id, in_port=msg.in_port,
+            actions=actions)
         dp.send_msg(out)

a9a373bc7679c2769d90707af53011e9bbbb30e7
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-26 10:33:19 -0500

    Started to populate the code based o nthe Ryu documentation for the first use of Ryu in this project. This is located in mynetwork.py

--- python/mynetwork.py
+++ python/mynetwork.py
@@ -1,2 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #This is the first script to setting up my SDN with  RYU.
+from ryu.base import app_manager
+
+class L2Switch(app_manager.RyuApp):
+    def _init_(self, *args, **kwargs):
+       super(L2Switch, self)._init(*args, **kwargs)

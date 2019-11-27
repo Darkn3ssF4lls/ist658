1d12c59724cc43a453b58cbbb49dd86453e76b0d
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-26 21:20:12 -0500

    Continuing to update bash script to deploy ryu in python 2 instead of python3.

--- bash/deploy-sdn-controller.sh
+++ bash/deploy-sdn-controller.sh
@@ -2,7 +2,7 @@
 #This script deploys the packages required to run a RYU SDN controller
 sudo apt install python-pip python3-pip gcc python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev libevent-dev python-all-dev
 
-#Required packages and optional packages as slected by  https://ryu.readthedocs.io/en/latest/getting_started.html
+#Python3 - Required packages and optional packages as selected by  https://ryu.readthedocs.io/en/latest/getting_started.html
 sudo pip3 install ryu
 sudo pip3 install ncclient
 sudo pip3 install paramiko
@@ -15,3 +15,15 @@ sudo pip3 install oslo.config
 sudo pip3 install msgpack
 sudo pip3 install eventlet
 
+#Python2 - Required packages and optional packages as selected by  https://ryu.readthedocs.io/en/latest/getting_started.html
+sudo pip install ryu
+sudo pip install ncclient
+sudo pip install paramiko
+sudo pip install SQLAlchemy
+sudo pip install WeBOB
+sudo pip install tinyrpc
+sudo pip install routes
+sudo pip install ovs 
+sudo pip install oslo.config
+sudo pip install msgpack
+sudo pip install eventlet

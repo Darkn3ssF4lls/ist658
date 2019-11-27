850a653788b21792fe946785f1322a645e4c091a
darkn3ssf4lls <39456389+Darkn3ssF4lls@users.noreply.github.com>
2019-11-26 21:17:32 -0500

    Updating the bash script to shift gears to python 2. Seems to be too many errors in the python 3 release.

--- bash/deploy-sdn-controller.sh
+++ bash/deploy-sdn-controller.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #This script deploys the packages required to run a RYU SDN controller
-sudo apt install python3-pip gcc python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev libevent-dev python-all-dev
+sudo apt install python-pip python3-pip gcc python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev libevent-dev python-all-dev
 
 #Required packages and optional packages as slected by  https://ryu.readthedocs.io/en/latest/getting_started.html
 sudo pip3 install ryu
